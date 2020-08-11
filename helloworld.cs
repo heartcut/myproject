4dc81dbb8240bc9159065393085b63d4836bf9d0
dameronjesse@gmail.com <dameronjesse@gmail.com>
2020-08-10 22:04:20 -0500

    my first change to the new branch

--- helloworld.cs
+++ helloworld.cs
@@ -12,6 +12,17 @@ namespace gittest
             Console.WriteLine("hey");
 
             //this is me testing git ??
+
+            //on my first branch ill start adding more code
+            var a = 1;
+            var b = 2;
+            var jesse = "jesse is my name";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(a+b);
+            Console.WriteLine(jesse);
+
         }
     }
 }
